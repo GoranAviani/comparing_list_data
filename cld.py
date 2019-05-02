657f3a2179d25356bf5f8a57242a8354e3d04163
goranaviani <goranaviani@gmail.com>
2019-05-02 17:38:07 +0200

    comparing 2 order lists

--- cld.py
+++ cld.py
@@ -1,11 +1,21 @@
 def main():
 
-    theirOrderIDs = []
-    ourOrderIDs = []
+    theirOrderIDs = [1,2,3]
+    ourOrderIDs = [1,2,3,4]
 
     theirSubsIDs = []
     ourSubsIDs = []
 
+
+    for x in ourOrderIDs:
+        if x not in theirOrderIDs:
+            print("Order {} is not in their list" .format(x))
+    print("------------------------------------------------------------------")
+
+    for x in theirOrderIDs:
+        if x not in ourOrderIDs:
+            print("Order {} is not in their list".format(x))
+    print("------------------------------------------------------------------")
 
 
 if __name__ == "__main__":
