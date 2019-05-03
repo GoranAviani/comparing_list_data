04d33c07ac801ae25e0448d94fa2799e5f844e49
goranaviani <goranaviani@gmail.com>
2019-05-03 17:24:40 +0200

    main menu with exit option

--- cld.py
+++ cld.py
@@ -1,13 +1,33 @@
+def mainMenu():
+
+    while True:
+        userInput = input("What do you want to do:\n"
+              "1 Import 2 lists and check their differences? or \n"
+              "2 Check differences between lists from the cld file?\n"
+              "----------------------------------------------------------\n"
+              "Press X to Exit")
+
+        # On exit show current stock and break
+        if userInput == "X":
+
+            print("Good bye !")
+            break
+
+
+
 def main():
 
+    mainMenu()
+
+
     theirCustIDs = []
-
-    ourCustIDs = [1,2,3,4]
-
+    ourCustIDs = []
     theirSubsIDs = []
     ourSubsIDs = []
 
 
+    list415 = []
+    list407 = []
     for x in ourCustIDs:
         if x not in theirCustIDs:
             print("Customer {} is not in their list" .format(x))
@@ -19,5 +39,17 @@ def main():
     print("------------------------------------------------------------------")
 
 
+
+    for x in list415:
+        if x not in list407:
+            print("Customer {} is not in their list407" .format(x))
+    print("------------------------------------------------------------------")
+
+
+
+
+
 if __name__ == "__main__":
     main()
+
+
