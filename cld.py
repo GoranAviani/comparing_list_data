d16f7852bc7132fa0ff49a4f23e6cb84addfe44d
goranaviani <goranaviani@gmail.com>
2019-05-03 17:31:28 +0200

    Simple side menu and lists search

--- cld.py
+++ cld.py
@@ -1,20 +1,31 @@
-def mainMenu():
+def checkCustomDiff():
+    userList1 = input("Please enter a list with (,) between words or numbers and press enter to enter second list: \n")
+    userList2 = input("Please enter a list with (,) between words or numbers and press enter: \n")
 
+    for x in userList1:
+        if x not in userList2:
+            print("{} is not in second list".format(x))
+    print("------------------------------------------------------------------")
+
+    for x in userList2:
+        if x not in userList1:
+            print("{} is not in first list".format(x))
+    print("------------------------------------------------------------------")
+
+
+def mainMenu():
     while True:
         userInput = input("What do you want to do:\n"
               "1 Import 2 lists and check their differences? or \n"
               "2 Check differences between lists from the cld file?\n"
               "----------------------------------------------------------\n"
               "Press X to Exit")
-
         # On exit show current stock and break
         if userInput.upper() == "X":
-
             print("Good bye !")
             break
-
-
-
+        elif userInput == '1':
+            checkCustomDiff()
 def main():
 
     mainMenu()
